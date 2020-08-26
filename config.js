e372e86d580a347f15d22d4d099a3640f242cf19
Allan Dumaine <ADumaine@users.noreply.github.com>
2020-08-26 00:23:21 -0700

    Change explorer urls to horizen.io

--- config.js
+++ config.js
@@ -1,13 +1,13 @@
 exports.config = {
   testnet: {
-    explorerURL: 'https://explorer-testnet.horizen.global/api',
+    explorerURL: 'https://explorer-testnet.horizen.io/api',
     server: 'https://securenodes.testnet.zensystem.io',
     anchorBlock: 677000,
     anchorHash: '0008d95b1d2631edaffaaacb1afa43e1ebca97347c682795b042fda1b09a848c',
     defaultFee: 0.0001,
   },
   mainnet: {
-    explorerURL: 'https://explorer.horizen.global/api',
+    explorerURL: 'https://explorer.horizen.io/api',
     server: 'https://supernodes.zensystem.io',
     anchorBlock: 760000,
     anchorHash: '0000000004ae3b44b9643ef95b09f980cf11f149b902217ccbcdcf72984a2990',
